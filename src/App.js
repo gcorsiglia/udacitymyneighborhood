4afa1b78719d1e5662a47c3008d1832e2dad50c7
Gina Corsiglia <gina@ginasaysyes.com>
2018-10-01 16:56:46 -0700

    Moved functions out of App component

--- src/App.js
+++ src/App.js
@@ -9,69 +9,21 @@ class App extends Component {
     super(props);
 
     this.state = {
-      venues: []
+      venues: [], 
+      sidebarIsOpen: false,
+      query: '',
+      markers: []
     }
   }
-
-  componentDidMount() {
-    this.getVenues();
+  
+  // Update venues list from props
+  newVenues = (venues) => {
+    this.setState({ venues });
   }
 
-  renderMap = () => {
-    // Load Google Maps Script after component mounts
-    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAV1AdfYz-I6GLGa5tFsJV3mpnc8pVeiVY&callback=initMap');
-
-    // Connect initMap() to global window so Maps API can use it
-    window.initMap = this.initMap;
-  }
-
-  // Get venues with Foursquare API 
-  getVenues = () => {
-    // Set search param
-    const endpoint = new URL('https://api.foursquare.com/v2/venues/explore?'),
-      params = {
-        client_id: 'D44WA1DU2RWYEEAKMRPLLYUX22001UY3EU3MQ1ZEADN433ZK',
-        client_secret: '3WK2RKHNWYOPX5CLRFRUFWTLY5Y31A2Q3EIXE1ES1EXDTFAK',
-        v: '20180323',
-        near: 'Port Townsend, WA',
-        query: 'food'
-    }
-    
-    // Convert param into URL for Fetch API
-    Object.keys(params)
-      .forEach(key => endpoint.searchParams.append(key, params[key]));
-    
-    // Fetch Foursquare data
-    fetch(endpoint)
-      .then(response => response.json())
-        // Set state with response data
-        .then(data => {
-          const results = data.response.groups[0].items;
-
-          this.setState({
-            venues: results
-          }, this.renderMap())
-        })
-        .catch(error => console.log(error));
-  }
-
-  initMap = () => {
-    // Create map
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 48.115501, lng: -122.775156},
-      zoom: 13
-    });
-
-    // Create markers
-    this.state.venues.map(venueItem => {
-      new window.google.maps.Marker({
-        position: {
-          lat: venueItem.venue.location.lat, 
-          lng: venueItem.venue.location.lng
-        },
-        map: map
-      })
-    })  
+  // Push new marker to state from props
+  addMarker = (marker) => {
+    this.state.markers.push(marker);
   }
 
   render() {
@@ -80,21 +32,17 @@ class App extends Component {
         <HeaderNav />
 
         <main className="main">
-          <MapContainer />
+          <MapContainer
+            venues={this.state.venues}
+            query={this.state.query}
+            newVenues={this.newVenues}
+            addMarker={this.addMarker}
+          />
         </main>
     </div> 
 
     );
   }
-}
-
-function loadScript(url) {
-  const index = window.document.getElementsByTagName('script')[0];
-  const script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
