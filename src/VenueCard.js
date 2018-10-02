832e215304d14f8c487412afdd8fa978cdde2629
Gina Corsiglia <gina@ginasaysyes.com>
2018-10-01 20:01:26 -0700

    VenueCard component added

---
+++ src/VenueCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class VenueCard extends Component {
+
+  render() {
+		
+		const { venue } = this.props;
+
+    return (
+    	<div className="venue-card">
+    	</div>
+    );
+  }
+}
+
+export default VenueCard;
