a1a34af61be0624001595fd7610c310888501a1b
Gina Corsiglia <gina@ginasaysyes.com>
2018-10-02 20:23:35 -0700

    Streetview API and image added; not pulling images correctly tho

--- src/VenueCard.js
+++ src/VenueCard.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 
 class VenueCard extends Component {
 
+	constructor(props) {
+    super(props);
+
+    this.getStreetviewImage = this.getStreetviewImage.bind(this);
+  }
+	
+	
+	getStreetviewImage = (venue) => {
+    /*
+     return `https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${this.props.venueDetails.location.lat},${this.props.venueDetails.location.lng}&key=AIzaSyAV1AdfYz-I6GLGa5tFsJV3mpnc8pVeiVY&signature=G4raxM-9zMlOmmrCaHJNlPCSNSI=` */
+
+    const baseStreetview = 'https://maps.googleapis.com/maps/api/streetview?'
+    
+    const params = {
+      size: '200x200',
+      location: `${this.props.venueDetails.venue.location.lat}, ${this.props.venueDetails.venue.location.lng}`,
+      key: 'AIzaSyAV1AdfYz-I6GLGa5tFsJV3mpnc8pVeiVY'
+      // signature: 'Sq48OclybOSukN34G3NXo4UMPK8='
+    }
+    
+    // Convert params into URL
+    const searchParams = Object.keys(params).map(k => `${k}=${params[k]}`).join('&');
+
+    return (baseStreetview + searchParams);
+  }
+  
+
+
+
   render() {
 		
 		const { venueDetails } = this.props;
@@ -13,8 +42,8 @@ class VenueCard extends Component {
         <p className="vc-address">{venueDetails.venue.location.formattedAddress[1]}</p>
         <div className="vc-details-body">
           <p>{venueDetails.venue.categories[0].name}</p>
-        </div>
-    		
+          <img src={this.getStreetviewImage(venueDetails)} width="200" height="200" />
+        </div>	
       </div>
     );
   }
