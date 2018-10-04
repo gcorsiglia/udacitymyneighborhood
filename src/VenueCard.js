14ff9e9185d4dd126ae2dd4ce3af279e9e10e292
Gina Corsiglia <gina@ginasaysyes.com>
2018-10-03 19:46:40 -0700

    Cleaned up code a bit

--- src/VenueCard.js
+++ src/VenueCard.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 
 class VenueCard extends Component {
-
-	constructor(props) {
-    super(props);
-  }
 	
+	// Get street view image
 	getStreetviewImage = (venue) => {
     
-    /*
-     return `https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${this.props.venueDetails.location.lat},${this.props.venueDetails.location.lng}&key=AIzaSyAV1AdfYz-I6GLGa5tFsJV3mpnc8pVeiVY&signature=G4raxM-9zMlOmmrCaHJNlPCSNSI=` */
-
     const baseStreetview = 'https://maps.googleapis.com/maps/api/streetview?'
     
     const params = {
@@ -28,30 +22,6 @@ class VenueCard extends Component {
     return (baseStreetview + searchParams);
   }
   
-  /*
-  getPhotos = () => {
-    
-		const photoUrl = `https://api.foursquare.com/v2/venues/${this.props.venueDetails.venue.id}/photos?client_id=D44WA1DU2RWYEEAKMRPLLYUX22001UY3EU3MQ1ZEADN433ZK&client_secret=3WK2RKHNWYOPX5CLRFRUFWTLY5Y31A2Q3EIXE1ES1EXDTFAK&v=20181003`;
-    
-    fetch(photoUrl)
-      .then(response => response.json())
-        // Set state with response data
-        .then(data => {
-
-          const prefix = data.response.photos.items[0].prefix;
-
-          const suffix = data.response.photos.items[0].suffix;
-
-          return prefix, suffix
-        })
-        .catch(error => console.log('Foursquare API error! ', error));
-		
-		return (prefix + 'cap300' + suffix)
-
-  } */
-
-
-
   render() {
 		
 		const { venueDetails } = this.props;
